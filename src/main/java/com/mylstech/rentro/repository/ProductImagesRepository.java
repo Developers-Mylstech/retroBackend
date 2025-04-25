7c36e04907bfdaaae4eb8bdc69856c7ed41545e9
uzair <uzairuddin7470@gmail.com>
2025-04-25 17:13:24 +0530

    remove productimages related fields and add single imagehandler

--- src/main/java/com/mylstech/rentro/repository/ProductImagesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mylstech.rentro.repository;
-
-import com.mylstech.rentro.model.ProductImages;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductImagesRepository extends JpaRepository<ProductImages, Long> {
-}
