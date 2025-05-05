584bcfa9f002576454cdf488036487dd9fa5317c
uzair <uzairuddin7470@gmail.com>
2025-05-05 14:02:20 +0530

    commit

--- src/main/java/com/mylstech/rentro/repository/ProductRepository.java
+++ src/main/java/com/mylstech/rentro/repository/ProductRepository.java
@@ -2,8 +2,22 @@ package com.mylstech.rentro.repository;
 
 import com.mylstech.rentro.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Find products available for sell
+    @Query("SELECT p FROM Product p WHERE p.productFor.sell IS NOT NULL")
+    List<Product> findByProductForSellNotNull();
+    
+    // Find products available for rent
+    @Query("SELECT p FROM Product p WHERE p.productFor.rent IS NOT NULL")
+    List<Product> findByProductForRentNotNull();
+    
+    // Find products available for service
+    @Query("SELECT p FROM Product p WHERE p.productFor.services IS NOT NULL")
+    List<Product> findByProductForServicesNotNull();
 }
