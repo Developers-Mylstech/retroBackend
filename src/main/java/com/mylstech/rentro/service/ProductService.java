3f05b8d5908ec1a989b9d6806dbfc5139f32df01
uzair <uzairuddin7470@gmail.com>
2025-05-05 14:03:21 +0530

    commit

--- src/main/java/com/mylstech/rentro/service/ProductService.java
+++ src/main/java/com/mylstech/rentro/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.mylstech.rentro.service;
 
+import com.mylstech.rentro.dto.request.BuyNowRequest;
 import com.mylstech.rentro.dto.request.ProductRequest;
+import com.mylstech.rentro.dto.response.CheckOutResponse;
 import com.mylstech.rentro.dto.response.ProductResponse;
+import com.mylstech.rentro.util.ProductType;
 
 import java.util.List;
 
@@ -15,4 +18,15 @@ public interface ProductService {
     // Additional methods for managing product images
     ProductResponse addImageToProduct(Long productId, String imageUrl);
     ProductResponse removeImageFromProduct(Long productId, String imageUrl);
+    
+    // New method to get products by type
+    List<ProductResponse> getProductsByType(ProductType productType);
+    
+    /**
+     * Buy a product immediately and proceed to checkout
+     * @param productId the product ID
+     * @param request the buy now request
+     * @return the checkout response
+     */
+    CheckOutResponse buyNow(Long productId, BuyNowRequest request);
 }
